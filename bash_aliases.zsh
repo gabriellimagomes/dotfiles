09ade9edc342d56bd2efbfa8430d939d5488d6b5
Gabriel Lima Gomes <gabriel.lima.ads@gmail.com>
2017-02-17 17:16:04 -0200

    adiciona arquivo conf do tmux e cria arquivo com alias importantes

---
+++ bash_aliases.zsh
@@ -0,0 +1,7 @@
+alias me="mvn eclipse:clean eclipse:eclipse"
+alias rehash="source ~/.zshrc"
+alias vpn-elo7="sudo openvpn ~/dev/vpn/gabriel.gomes-lin.ovpn"
+alias ge="gradle cleanEclipse eclipse"
+alias gi="gradle clean install"
+alias pl="ps aux | grep"
+
