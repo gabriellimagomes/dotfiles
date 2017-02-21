1aba13506dfce9be8988a50cd5e9ee0aa64bb7cd
Gabriel Lima Gomes <gabriel.lima.ads@gmail.com>
2017-02-21 14:55:38 -0300

    add alias for ssh

--- bash_aliases.zsh
+++ bash_aliases.zsh
@@ -4,4 +4,6 @@ alias vpn-elo7="sudo openvpn ~/dev/vpn/gabriel.gomes-lin.ovpn"
 alias ge="gradle cleanEclipse eclipse"
 alias gi="gradle clean install"
 alias pl="ps aux | grep"
+alias sshc="ssh -i ~/dev/keys/sa-default-key.pem -l core"
+alias sshu="ssh -i ~/dev/keys/sa-default-key.pem -l ubuntu"
 
